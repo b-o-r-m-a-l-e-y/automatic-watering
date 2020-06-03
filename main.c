2ac73c9e645763a5303e06bf0aeec26ce057dcec
b-o-r-m-a-l-e-y <evgening@mail.ru>
2020-06-03 11:11:43 -0400

    code

--- main.c
+++ main.c
@@ -2,9 +2,18 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define ADC_MAX 1024
+
 #define WATER_PUPM_PIN 3
 
+#define BATTERY_MASK          (0<<MUX1)|(1<<MUX0)
+#define SENSOR_MASK           (1<<MUX1)|(0<<MUX0)
+
+//Resistor divider Up = 680k and Down = 150k
+#define ADC_MAX 1024
+#define BATTERY_LOW_LIMIT   768 //The lowest voltage for battery = 3.4 Volts
+#define SOIL_LOW_LIMIT      800 //The lower, the more dusty soil is
+
+uint8_t waterPumpFlag = 0;
 
 void configuration()
 {
@@ -14,7 +23,7 @@ void configuration()
     PORTB = (1<<PORTB5);
 
     WDTCR = WDCE<<1;
-    //INT each 8s
+    //Watchdog interrupt each 8s
     WDTCR |= (1<<WDP3)|(0<<WDP2)|(0<<WDP1)|(1<<WDP0);
     //Enable WD interrupt
     WDTCR |= (1<<WDTIE);
@@ -24,6 +33,7 @@ void configuration()
     //Enable ADC
     ADCSRA |= (1<<ADEN);
     ADCSRA &= ~(1<<ADIF);
+    sei();
 }
 
 uint16_t getADCResult(uint8_t mask)
@@ -38,13 +48,27 @@ uint16_t getADCResult(uint8_t mask)
     return data;
 }
 
+void batteryCheck()
+{
+    uint16_t batVoltage = 0;
+    batVoltage = getADCResult(BATTERY_MASK);
+    if (batVoltage<BATTERY_LOW_LIMIT) PORTB |= (1<<PORTB0);
+    else PORTB &= (~1<<PORTB0);
+}
+
+void soilSensorCheck()
+{
+    uint16_t soilVoltage = 1024;
+    soilVoltage = getADCResult(SENSOR_MASK);
+    if (soilVoltage<SOIL_LOW_LIMIT) waterPumpFlag=1;
+}
 
 int main()
 {
     configuration();
     while(1)
     {
-        
+        batteryCheck();        
     }
     return 0;
 }
