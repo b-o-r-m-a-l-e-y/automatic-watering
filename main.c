0c3f3950fe5b5e94665b8399cecd5cfcc5f0e46c
b-o-r-m-a-l-e-y <evgening@mail.ru>
2020-06-07 21:25:32 +0300

    add timer service

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+//Freq CPUio 9.6 MHz/8=1.2MHz
 
 #define WATER_PUPM_PIN 3
 
@@ -10,10 +11,19 @@
 
 //Resistor divider Up = 680k and Down = 150k
 #define ADC_MAX 1024
-#define BATTERY_LOW_LIMIT   768 //The lowest voltage for battery = 3.4 Volts
-#define SOIL_LOW_LIMIT      800 //The lower, the more dusty soil is
+#define BATTERY_LOW_LIMIT       768 //The lowest voltage for battery = 3.4 Volts
+#define SOIL_LOW_LIMIT          800 //The lower, the more dusty soil is
+
+#define WATER_PUMP_TIME_MAX     4000 //How much time water pump os on
 
 uint8_t waterPumpFlag = 0;
+volatile uint16_t msCounter = 0;
+volatile uint16_t waterPumpTime = 0;
+
+void waterPumpTimerService()
+{
+
+}
 
 void configuration()
 {
@@ -33,6 +43,16 @@ void configuration()
     //Enable ADC
     ADCSRA |= (1<<ADEN);
     ADCSRA &= ~(1<<ADIF);
+
+    //Timer for 1 ms CTC Mode
+    //Enable interrupt on Output Compare Match A
+    TIMSK0=(1<<OCIE0A);
+    TCCR0A=(1<<WGM01)|(0<<WGM00);
+    //Prescaler 256
+    TCCR0B=(1<<CS02);
+    TCNT0=0;
+    OCR0A=47;
+    
     sei();
 }
 
@@ -48,6 +68,9 @@ uint16_t getADCResult(uint8_t mask)
     return data;
 }
 
+/*
+ * Checking battery voltage. If low, light up LED on PB0
+ */
 void batteryCheck()
 {
     uint16_t batVoltage = 0;
@@ -68,7 +91,21 @@ int main()
     configuration();
     while(1)
     {
-        batteryCheck();        
+        batteryCheck();
+        if(waterPumpFlag)
+        {
+            PORTB |=(1<<PORTB3);
+        }        
     }
     return 0;
+}
+
+ISR(TIM0_COMPA_vect)
+{
+    msCounter++;
+    if(waterPumpFlag)
+    {
+        waterPumpTime++;
+    }
+    if(waterPumpTime>=WATER_PUMP_TIME_MAX) waterPumpFlag=0;
 }
